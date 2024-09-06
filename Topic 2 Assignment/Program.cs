fbd844899933bd9b6cb7b0e4ef98fb52c9aab78b
339719510 <339719510@2260I-L29-004>
2024-09-06 09:34:58 -0400

    Add project files.

---
+++ Topic 2 Assignment/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Topic_2_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {//Bailey made this program
+
+            
+
+            Console.Write("Year I was Born: ");
+            Console.WriteLine(2024 - 16);
+            Console.WriteLine(" ");
+            Console.Write("The Average of 14, 15, and 16 is ");
+            Console.WriteLine((14 + 15 + 16) / 3);
+            Console.WriteLine(" ");
+            Console.Write("The Average of 14, 15, 16, and 17 is ");
+            Console.WriteLine((14.0 + 15.0 + 16.0 + 17.0) / 4.0);
+            Console.WriteLine(" ");
+            Console.Write("The Area of a Circle is ");
+            Console.Write(3.1415926 * (3 * 3));
+            Console.WriteLine(" if the Radius is 3cm");
+            Console.WriteLine(" ");
+            Console.WriteLine("C# knows to add with the + operator ony if you put brackets around the non-strings");
+            Console.WriteLine(" ");
+            Console.WriteLine("For Example:");
+            Console.WriteLine(" ");
+            Console.WriteLine("Bailey" + 7 + 2);
+            Console.WriteLine(" ");
+            Console.WriteLine("Bailey" + (7 + 2));
+            
+            Console.ReadLine();
+        }
+    }
+}
